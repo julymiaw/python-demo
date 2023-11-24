4df9f3a309177b94125485a9ed2853db3ccedee5
julymiaw <julywu399@gmail.com>
2023-11-24 17:26:56 +0800

    first commit

--- api.py
+++ api.py
@@ -1,18 +1,41 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
+from flask_cors import cross_origin
 from OpenReview import func
+from wordcloud import WordCloud
+from io import BytesIO
 
 app = Flask(__name__)
-data = None  # 用于存储数据的变量
+data = None
 
 @app.route('/api/data', methods=['GET'])
+@cross_origin()
 def get_data():
-    global data
-    mode = int(request.args.get('mode', 1))  # 获取页码参数，默认为1
+    mode = int(request.args.get('mode'))
 
-    if data is None:  # 如果数据还没有被爬取
-        data = func(mode)
+    global data
+    data = func(mode)
 
     return jsonify(data)  # 返回指定页的数据
+
+@app.route('/api/keywords-wordcloud', methods=['GET'])
+@cross_origin()
+def generate_keywords_wordcloud():
+    global data
+    flat_list = []
+    for item in data['keywords']:
+        for words in item:
+            flat_list.append(words)
+    keywords_text = ",".join(flat_list)
+
+    # 生成词云
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(keywords_text)
+
+    # 保存词云为图片
+    img_buffer = BytesIO()
+    wordcloud.to_image().save(img_buffer, format='PNG')
+    img_buffer.seek(0)
+
+    return send_file(img_buffer, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(port=5000)
--- homepage.js
+++ homepage.js
@@ -1,29 +1,80 @@
 const loading = document.getElementById('loading');
-
-function initPage(){
-    getData();
-}
+let data;
 
 function clickTab(x) {
-    let elem = document.getElementById("tab_"+x)
-    if(elem.style.color==="rgb(18, 18, 18)")
-        elem.style.color="#056de8";
-    for(let i=0;i<5;i++){
-        let temp = document.getElementById("tab_"+i)
-        if(i!=x && temp.style.color==="rgb(5, 109, 232)")
-            temp.style.color="#121212";
+    let elem = document.getElementById("tab_" + x)
+    if (elem.style.color === "rgb(18, 18, 18)")
+        elem.style.color = "#056de8";
+    for (let i = 0; i < 5; i++) {
+        let temp = document.getElementById("tab_" + i)
+        if (i !== x && temp.style.color === "rgb(5, 109, 232)")
+            temp.style.color = "#121212";
     }
+    loading.style.display = 'flex';
+    getData(x).then((d) => {
+        data = d;
+        console.log(data);
+        displayPage();
+    });
 }
 
-async function getData() {
-    const mode = 0;
+async function getData(mode) {
     try {
         const response = await fetch('http://localhost:5000/api/data?mode=' + mode);
         const data = await response.json();
         // 使用返回的数据渲染页面
-        console.log(data);
         loading.style.display = 'none';
+        return data;
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+function displayPage() {
+    if (data.length === 0) console.log("获取文章失败");
+    const container = document.querySelector('.article-container');
+    if (container) {
+        container.innerHTML = '';
+        getKeywordsWordcloud()
+        // 遍历数据数组，渲染每个文章块
+        for (let i = 0; i < data.authors.length; i++) {
+            // 创建一个新的文章块元素
+            let article = document.createElement('div');
+            article.className = 'article';
+
+            // 填充文章块的内容
+            article.innerHTML = `
+                <h2>${data.titles[i]}</h2>
+                <p>Author: ${data.authors[i]}</p>
+                <p>Keywords: ${data.keywords[i]}</p>
+                <a href="${data.pdfs[i]}" target="_blank">Download PDF</a>
+            `;
+
+            // 将文章块添加到 article-container 中
+            container.appendChild(article);
+        }
+    } else {
+        console.error('Article container not found!');
+    }
+}
+
+function initPage() {
+    clickTab(0)
+}
+
+function getKeywordsWordcloud() {
+    const container = document.querySelector('.img-container');
+    container.innerHTML = '';
+    fetch(`http://localhost:5000/api/keywords-wordcloud`)
+        .then(response => response.blob())
+        .then(blob => {
+            // 创建一个表示图片的URL
+            let imageUrl = URL.createObjectURL(blob);
+
+            // 在页面上显示词云图片
+            let imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+            container.appendChild(imgElement);
+        })
+        .catch(error => console.error('Error fetching wordcloud:', error));
 }
